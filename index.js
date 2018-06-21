c427356321b04937c50c448b5263af2eb40892e4
Kelsey Fecho <kfecho30@gmail.com>
2018-06-21 16:42:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ function produceDrivingRange() {
 function createDriver(){
   let driverId = 0;
   return class Driver {
-    constructor(){
+    constructor(name){
+      this.name = name;
       this.id = ++driverId
     }
   }
